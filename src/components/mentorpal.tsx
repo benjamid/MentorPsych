785c39d8c9ac18d59f86a5a18419ab2431c769c9
Brian Chung <35940686+BrChung@users.noreply.github.com>
2020-07-29 12:24:30 -0700

    Update mentorpal.tsx

--- src/components/mentorpal.tsx
+++ src/components/mentorpal.tsx
@@ -1,39 +1,15 @@
-import React, { useRef } from "react"
+import React from "react"
 import withLocation from "./withLocation"
 
 const MentorPal = ({ search }) => {
-  const { email } = search
-
-  const mentorPalRef = useRef(null)
-
-  const mentorLoadEvent = () => {
-    if (email) {
-      setTimeout(iFrameDom, 2000)
-    }
-  }
-
-  const iFrameDom = () => {
-    // SecurityError: Blocked a frame with origin "http://localhost:8000" from accessing a cross-origin frame.
-    // https://stackoverflow.com/questions/25098021/securityerror-blocked-a-frame-with-origin-from-accessing-a-cross-origin-frame
-    const inputField = mentorPalRef.current.contentWindow.document.getElementById(
-      "guest-prompt-input"
-    )
-    const sendButton = mentorPalRef.current.contentWindow.document.getElementById(
-      "guest-prompt-input-send"
-    )
-    if (inputField && sendButton) {
-      inputField.value = email
-      sendButton.click()
-    }
-  }
-
+  const { uuid } = search
+  const src =
+    "https://mentorpal.org/mentorpanel/" + (uuid ? `?guest=${uuid}` : "")
   return (
     <iframe
-      src="https://mentorpal.org/mentorpanel/?mentor=clint"
+      src={src}
       title="MentorPal Test"
       className="mentorpal-frame"
-      onLoad={mentorLoadEvent}
-      ref={mentorPalRef}
     ></iframe>
   )
 }
