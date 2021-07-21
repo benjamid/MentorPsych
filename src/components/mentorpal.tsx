871d1fb883b8b7e24fcf0c1f51aeda3ddef8d1fb
Kevin Mai <kevinmai@Kevins-MacBook-Pro.local>
2021-07-20 23:52:27 -0700

    Code Update

--- src/components/mentorpal.tsx
+++ src/components/mentorpal.tsx
@@ -4,7 +4,8 @@ import withLocation from "./withLocation"
 const MentorPal = ({ search }) => {
   const { uuid } = search
   const src =
-    "https://mentorpal.org/mentorpanel/" + (uuid ? `?guest=${uuid}` : "")
+     "https://mentorpal.org/mentorpanel/?mentor=trevitt-jennifer-prof-psych" + (uuid ? '&guest=${uuid}':"")
+    //"https://mentorpal.org/mentorpanel/" + (uuid ? `?guest=${uuid}` : "")
   return (
     <iframe
       src={src}
